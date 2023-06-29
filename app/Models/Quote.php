95ed4f110fce3e97b135152b393cd0578dff5063
rokiprasetyoadi <rokiprasetyoadi@gmail.com>
2023-06-29 23:16:45 +0700

    create Models

---
+++ app/Models/Quote.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quote extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'quote',
+        'author',
+        'user_id'
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
